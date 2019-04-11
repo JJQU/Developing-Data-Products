099f16422b00126a51ab5fcbca1325374ec74692
JJQU <37889191+JJQU@users.noreply.github.com>
2019-04-11 11:54:39 -0400

    Update server.R

--- server.R
+++ server.R
@@ -1,18 +1,21 @@
-#
-# This is the server logic of a Shiny web application. You can run the 
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 library(ggplot2)
+library(dplyr)
 
-# Define server logic required to draw a histogram
+
 shinyServer(function(input, output) {
+  datasetInput <- reactive({
+    switch(input$dataset,
+           "Apple" = apple2,
+           "Amazon" = amazon2,
+           "Facebook" = facebook2,
+           "Google" = google2)
+  })
    
+  output$view <- renderTable({
+    filter(datasetInput(), Date1 == input$date)
+  })
+  
   output$plot1 <- renderPlot({
     
     g <- ggplot(gafa_stock, aes(Date, Price, color = Company)) + geom_line() + xlab("Date") + ylab("Price")
@@ -20,4 +23,8 @@ shinyServer(function(input, output) {
     
   })
   
+  
 })
+
+
+
